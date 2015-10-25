d8fa69424a629874b6b72bc0ff167ab489446955
kimo5532 <shotbyabel@gmail.com>
2015-10-25 00:35:18 -0700

    import reddit JSON to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient
-  , request = require('request');
+  , request = require('request'); //nodeJs library for making request
 
 MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
     if(err) throw err;
